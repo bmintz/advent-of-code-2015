09ab10dac40fae2610482682c735fb3d27483771
bmintz <bmintz@users.noreply.github.com>
2017-12-03 00:31:51 -0600

    solve day 5 part one

--- 5/nice_strings.py
+++ 5/nice_strings.py
@@ -5,13 +5,23 @@ import sys
 
 from ben import input_iter
 
+def has_repeated_letter(s):
+	for i in range(len(s) - 1):
+		if s[i] == s[i+1]:
+			return True
+	return False
 
-def answer_one():
-	...
+
+def is_nice_one(string):
+	return (
+		sum([c in 'aeiou' for c in string]) >= 3
+		and has_repeated_letter(string)
+		and not any(s in string for s in ('ab', 'cd', 'pq', 'xy'))
+	)
 
 
-def answer_two():
-	...
+def answer(predicate):
+	return sum(predicate(s) for s in input_iter())
 
 
 def main():
@@ -20,9 +30,11 @@ def main():
 		sys.exit(1)
 	mode = sys.argv[1]
 	if mode == '1':
-		print(answer_one())
+		func = is_nice_one
 	elif mode == '2':
-		print(answer_two())
+		func = is_nice_two
+
+	print(answer(func))
 
 
 if __name__ == '__main__':
